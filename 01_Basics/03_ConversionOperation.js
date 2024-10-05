6915b31868765cf2a79b1e4d0c58df73f11d108c
SanketKaadam <109967338+SanketKaadam@users.noreply.github.com>
2024-10-05 18:55:58 +0000

    Operations

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,107 @@
+let score = "33"
+
+let numScore = Number(score)
+
+console.log(numScore);
+console.log(typeof numScore);
+
+let someNumber = 100
+
+let numberInString = String(someNumber)
+
+console.log(numberInString);
+console.log(typeof numberInString);
+
+let numWithString = "40asd"
+
+let numFromString = Number(numWithString)
+
+console.log(numFromString);
+console.log(typeof numFromString);
+
+let accountActive = 1
+
+let booleanAcActive = Boolean(accountActive)
+console.log(booleanAcActive);
+console.log(typeof booleanAcActive);
+
+
+let accountActive2 = "sdasda"
+
+let booleanAcActive2 = Boolean(accountActive2)
+console.log(booleanAcActive2);
+console.log(typeof booleanAcActive2);
+
+// --------------- Notes ----------------------
+
+let score1 = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score1)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber4 = 33
+
+let stringNumber = String(someNumber4)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
