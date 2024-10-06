58b1ab9d0e778d11f70ccc53f8388b21246c881c
SanketKaadam <109967338+SanketKaadam@users.noreply.github.com>
2024-10-06 05:28:15 +0000

    String Methods

---
+++ 01_Basics/07_String.js
@@ -0,0 +1,17 @@
+let acName = "sanket"
+let age = 25
+
+console.log(`Hi, My name is ${acName} and I am ${age} years old.`);
+
+let gameName = "Sanket-k-Kadam"
+
+console.log(gameName.length);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('n'));
+console.log(gameName.includes('ket'));
+console.log(gameName.split('-'));
+console.log(gameName.replace('t', 'tttt'));
+console.log(gameName.substring(0,5));
+
+console.log(gameName);
+
