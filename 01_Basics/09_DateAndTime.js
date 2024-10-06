070edb43889e22d005bc9de2aadb4f728ca324a0
SanketKaadam <109967338+SanketKaadam@users.noreply.github.com>
2024-10-06 10:03:04 +0000

    Date and Time

---
+++ 01_Basics/09_DateAndTime.js
@@ -0,0 +1,30 @@
+let currentDate = new Date();
+// console.log(currentDate);
+// console.log(currentDate.getDate());
+// console.log(currentDate.toLocaleDateString());
+// console.log(currentDate.toDateString());
+// console.log(currentDate.toTimeString());
+
+let myCreatedDate = new Date(2024, 0, 25 )
+let myCreatedDate2 = new Date(2024, 0, 25, 5, 3 )
+let myCreatedDate3 = new Date("2023-01-14")
+let myCreatedDate4 = new Date("01-14-2023")
+
+// console.log(myCreatedDate);
+// console.log(myCreatedDate2);
+// console.log(myCreatedDate3.toLocaleString());
+// console.log(myCreatedDate4.toLocaleDateString());
+
+let currentTime = Date.now()
+console.log(currentTime);
+console.log(currentDate.getTime());
+console.log(Math.floor(currentTime/1000));
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
+
+
